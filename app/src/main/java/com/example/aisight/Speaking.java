2f81a1c9c2d6c2a27f1edb6e743a4624e32e2820
Devanshu Jha <devanshujha8305@gmail.com>
2023-06-14 16:12:43 +0530

    initial commit

---
+++ app/src/main/java/com/example/aisight/Speaking.java
@@ -0,0 +1,73 @@
+package com.example.aisight;
+
+import android.app.Activity;
+import android.speech.tts.TextToSpeech;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+
+public class Speaking {
+    TextToSpeech textToSpeech;
+
+    public Speaking(Activity caller, String text) {
+        textToSpeech = new TextToSpeech(caller.getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+                if(i!=TextToSpeech.ERROR){
+                    // To Choose language of speech
+                    textToSpeech.setLanguage(Locale.getDefault()); // sets language of device
+                    say(text);
+                }
+            }
+        });
+    }
+
+    public Speaking(LocationService parent, String text) {
+        textToSpeech = new TextToSpeech(parent.getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+                if(i!=TextToSpeech.ERROR){
+                    // To Choose language of speech
+                    textToSpeech.setLanguage(Locale.getDefault()); // sets language of device
+                    say(text);
+                }
+            }
+        });
+    }
+
+    public Speaking(FloatingOverMapIconService parent, String text) {
+        textToSpeech = new TextToSpeech(parent.getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+                if(i!=TextToSpeech.ERROR){
+                    // To Choose language of speech
+                    textToSpeech.setLanguage(Locale.getDefault()); // sets language of device
+                    say(text);
+                }
+            }
+        });
+    }
+
+    public Speaking(@NotNull BackgroundObjectDetectionService parent, @NotNull String text) {
+        textToSpeech = new TextToSpeech(parent.getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+                if(i!=TextToSpeech.ERROR){
+                    // To Choose language of speech
+                    textToSpeech.setLanguage(Locale.getDefault()); // sets language of device
+                    say(text);
+                }
+            }
+        });
+    }
+
+    private void say(String text) {
+        textToSpeech.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+    }
+
+}
